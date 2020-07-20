c070c1560c065f62cbfefdf019565aaa64bc768e
Mayank Aggarwal <mayank2aggarwal@gmail.com>
2020-07-20 19:35:03 +0530

    feat: provide movement functionality to the ship

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pygame
 import os
 import time
 import random
+pygame.font.init()
 
 # Load Enemy Ships
 RED_SPACE_SHIP = pygame.image.load(os.path.join('assets', 'pixel_ship_red_small.png'))
@@ -20,20 +21,52 @@ YELLOW_LASER = pygame.image.load(os.path.join('assets', 'pixel_laser_yellow.png'
 # Load Background Image
 backgroundImage = pygame.image.load(os.path.join('assets', 'background-black.png'))
 
-WIDTH, HEIGHT = 700, 700
+# Canvas Dimensions
+WIDTH, HEIGHT = 600, 600
 CANVAS = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption('SPACE INVADERS')
 
 # Set Background Dimensions
 BG = pygame.transform.scale(backgroundImage, (WIDTH, HEIGHT))
 
+class Ship:
+    def __init__(self, x, y, health=100):
+        self.x = x
+        self.y = y
+        self.health = health
+        self.ship_img = None
+        self.laser_img = None
+        self.lasers = []
+        self.cool_down_counter = 0
+        self.width = 50
+        self.height = 50
+
+    def draw(self, window):
+        pygame.draw.rect(window, (255, 0, 0), (self.x, self.y, self.width, self.height))
+
 def main():
     run = True
     FPS = 60
+    lives = 5
+    level = 1
+    player_vel = 5
+    main_font = pygame.font.SysFont('comicsans', 35)
+
+    ship = Ship(WIDTH//2 - 25, 500)
+
     clock = pygame.time.Clock()
 
     def redraw_window():
         CANVAS.blit(BG, (0, 0))
+        # Draw Text
+        level_label = main_font.render(f'Level: {level}', 1, (255, 255, 255))
+        lives_label = main_font.render(f'Lives: {lives}', 1, (255, 255, 255))
+
+        CANVAS.blit(level_label, (WIDTH - level_label.get_width() - 10, 10))
+        CANVAS.blit(lives_label, (10, 10))
+
+        ship.draw(CANVAS)
+
         pygame.display.update()
 
     while run:
@@ -43,5 +76,16 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
+
+        keys = pygame.key.get_pressed()
+        if (keys[pygame.K_LEFT] or keys[pygame.K_a]) and (ship.x - player_vel) > 0:  # Left Key
+            ship.x -= player_vel
+        if (keys[pygame.K_RIGHT] or keys[pygame.K_d]) and (ship.x + player_vel + ship.width) < WIDTH:  # Right Key
+            ship.x += player_vel
+        if (keys[pygame.K_UP] or keys[pygame.K_w]) and (ship.y - player_vel) > 0:  # Up Key
+            ship.y -= player_vel
+        if (keys[pygame.K_DOWN] or keys[pygame.K_s]) and (ship.y + player_vel + ship.height) < HEIGHT:  # Down Key
+            ship.y += player_vel
+
 
 main()
