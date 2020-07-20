32b72d230602a8a05b75f97220b09e1801925bbf
Mayank Aggarwal <mayank2aggarwal@gmail.com>
2020-07-20 18:07:12 +0530

    feat: setup canvas

--- main.py
+++ main.py
@@ -3,10 +3,6 @@ import os
 import time
 import random
 
-WIDTH, HEIGHT = 750, 750
-CANVAS = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption('SPACE INVADERS')
-
 # Load Enemy Ships
 RED_SPACE_SHIP = pygame.image.load(os.path.join('assets', 'pixel_ship_red_small.png'))
 BLUE_SPACE_SHIP = pygame.image.load(os.path.join('assets', 'pixel_ship_blue_small.png'))
@@ -15,11 +11,37 @@ GREEN_SPACE_SHIP = pygame.image.load(os.path.join('assets', 'pixel_ship_green_sm
 # Load Player Ship
 YELLOW_SPACE_SHIP = pygame.image.load(os.path.join('assets', 'pixel_ship_yellow.png'))
 
-# LASERS
+# Load Lasers
 RED_LASER = pygame.image.load(os.path.join('assets', 'pixel_laser_red.png'))
 BLUE_LASER = pygame.image.load(os.path.join('assets', 'pixel_laser_blue.png'))
 GREEN_LASER = pygame.image.load(os.path.join('assets', 'pixel_laser_green.png'))
 YELLOW_LASER = pygame.image.load(os.path.join('assets', 'pixel_laser_yellow.png'))
 
-# BACKGROUND IMAGE
-BG = pygame.image.load(os.path.join('assets', 'background-black.png'))
+# Load Background Image
+backgroundImage = pygame.image.load(os.path.join('assets', 'background-black.png'))
+
+WIDTH, HEIGHT = 700, 700
+CANVAS = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption('SPACE INVADERS')
+
+# Set Background Dimensions
+BG = pygame.transform.scale(backgroundImage, (WIDTH, HEIGHT))
+
+def main():
+    run = True
+    FPS = 60
+    clock = pygame.time.Clock()
+
+    def redraw_window():
+        CANVAS.blit(BG, (0, 0))
+        pygame.display.update()
+
+    while run:
+        clock.tick(FPS)
+        redraw_window()
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+
+main()
