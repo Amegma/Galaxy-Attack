8ec1aa3147eaaa04713dc83277dfc96f1aeb61f8
ThomasChr <thomaschristlieb@hotmail.com>
2021-10-06 20:14:41 +0200

    move graphic assets to subfolder

--- constants.py
+++ constants.py
@@ -8,33 +8,33 @@ HEIGHT = 750
 score_list = []
 
 # Load Controls Image
-startImage = pygame.image.load(os.path.join('assets', 'play.png'))
-controlImage = pygame.image.load(os.path.join('assets', 'joystick.png'))
-trophyImage = pygame.image.load(os.path.join('assets', 'trophy.png'))
+startImage = pygame.image.load(os.path.join('assets', 'graphics', 'play.png'))
+controlImage = pygame.image.load(os.path.join('assets', 'graphics', 'joystick.png'))
+trophyImage = pygame.image.load(os.path.join('assets', 'graphics', 'trophy.png'))
 
 # Load Hearts
-heartImage = pygame.image.load(os.path.join('assets', 'heart.png'))
+heartImage = pygame.image.load(os.path.join('assets', 'graphics', 'heart.png'))
 
 # Load Boss Ship
-BOSS_SHIP = pygame.image.load(os.path.join('assets', 'boss.png'))
+BOSS_SHIP = pygame.image.load(os.path.join('assets', 'graphics', 'boss.png'))
 
 # Load Enemy Ships
-EASY_SPACE_SHIP = pygame.image.load(os.path.join('assets', 'easy.png'))
-MEDIUM_SPACE_SHIP = pygame.image.load(os.path.join('assets', 'medium.png'))
-HARD_SPACE_SHIP = pygame.image.load(os.path.join('assets', 'hard.png'))
+EASY_SPACE_SHIP = pygame.image.load(os.path.join('assets', 'graphics', 'easy.png'))
+MEDIUM_SPACE_SHIP = pygame.image.load(os.path.join('assets', 'graphics', 'medium.png'))
+HARD_SPACE_SHIP = pygame.image.load(os.path.join('assets', 'graphics', 'hard.png'))
 
 # Load Player
-PLAYER_SPACE_SHIP = pygame.image.load(os.path.join('assets', 'retro-spaceship.png'))
-PLAYER_LASER = pygame.image.load(os.path.join('assets', 'pixel_laser_cosmic.png'))
+PLAYER_SPACE_SHIP = pygame.image.load(os.path.join('assets', 'graphics', 'retro-spaceship.png'))
+PLAYER_LASER = pygame.image.load(os.path.join('assets', 'graphics', 'pixel_laser_cosmic.png'))
 
 # Load Lasers
-RED_LASER = pygame.image.load(os.path.join('assets', 'pixel_laser_red.png'))
-BLUE_LASER = pygame.image.load(os.path.join('assets', 'pixel_laser_blue.png'))
-GREEN_LASER = pygame.image.load(os.path.join('assets', 'pixel_laser_green.png'))
-PURPLE_LASER = pygame.image.load(os.path.join('assets', 'pixel_laser_purple.png'))
+RED_LASER = pygame.image.load(os.path.join('assets', 'graphics', 'pixel_laser_red.png'))
+BLUE_LASER = pygame.image.load(os.path.join('assets', 'graphics', 'pixel_laser_blue.png'))
+GREEN_LASER = pygame.image.load(os.path.join('assets', 'graphics', 'pixel_laser_green.png'))
+PURPLE_LASER = pygame.image.load(os.path.join('assets', 'graphics', 'pixel_laser_purple.png'))
 
 # Load Background Image
-backgroundImage = pygame.image.load(os.path.join('assets', 'background-black.png'))
+backgroundImage = pygame.image.load(os.path.join('assets', 'graphics', 'background-black.png'))
 
 # Set Background Dimensions
 BG = pygame.transform.scale(backgroundImage, (WIDTH, HEIGHT))
